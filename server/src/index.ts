47462235df444b4a259572084e5a1140f064c21d
caarlosdamian <dapecarlos@gmail.com>
2022-09-24 23:05:30 -0500

    add simple routing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,45 @@
-console.log('Hello world!')
+import express from "express";
+import mysql from "mysql";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "test",
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/books", (req, res) => {
+  db.query("SELECT * FROM books", (err, result) => {
+    if (err) {
+      res.json(err);
+    }
+    return res.send(result);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const query =
+    " INSERT INTO books (`title`, `description`,`cover`) VALUES (?);";
+  const values = [
+    "title from Backend",
+    "description from Backend",
+    "cover from Backend",
+  ];
+
+  db.query(query, [values], (err, data) => {
+    if (err) {
+      res.json(err);
+    }
+    return res.json("books has been created succesfully");
+  });
+});
+
+app.listen(5001, () => {
+  console.log("Server is running on port 5001");
+});
